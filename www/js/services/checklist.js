e4fc59f3aeaf1c0da3c054784312a9ff41d2f6d3
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-10-14 18:41:42 -0300

    fix service url, remove authentication headers

--- www/js/services/checklist.js
+++ www/js/services/checklist.js
@@ -12,14 +12,18 @@ altamiraApp.factory('ImportOrders', function($http, $log, $q) {
 			//get the data from api
 			$http({
 				method: 'GET', 
-				url: 'http://integracao.altamira.com.br/manufacturing/bom?'+orderNum,
-				headers: {'Authorization': 'Basic QWRtaW5pc3RyYXRvcjohYkZDWC45WCpUSg=='}
+				url: 'http://integracao.altamira.com.br/manufacturing/bom?' + orderNum,
+				crossDomain: true  
 			}).success(function(data) {
 				//post data to api
 				$http({
                     method: "post",
-					url: "http://data.altamira.com.br/sales/order",
-					data: data  
+					url: "http://data.altamira.com.br/data/rest/sales/order",
+					data: data,
+					xhrFields: {
+				       withCredentials: true
+				    },
+				    crossDomain: true  
 				}).success(function(data) {
 				
 				}).error(function(data, status) { // called asynchronously if an error occurs
@@ -28,7 +32,6 @@ altamiraApp.factory('ImportOrders', function($http, $log, $q) {
 			}).error(function(msg, code) {
 				deferred.reject(msg);
 				$log.error(msg, code);
-				
 			});
 			return deferred.promise;
 		}
